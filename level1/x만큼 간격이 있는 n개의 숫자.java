0b0917b32ae2634da74ebc04915e051b8724387a
SHIN CHANGYU <63400830+scv1702@users.noreply.github.com>
2023-07-09 18:40:48 +0900

    Improve x만큼 간격이 있는 n개의 숫자

--- level1/x만큼 간격이 있는 n개의 숫자.java
+++ level1/x만큼 간격이 있는 n개의 숫자.java
@@ -1,3 +1,5 @@
+/*
+time complexity: O(N^2)
 class Solution {
     public long[] solution(int x, int n) {
         long[] answer = new long[n];
@@ -5,6 +7,18 @@ class Solution {
             for (int j = i; j < n; j++) {
                 answer[j] += x;
             }
+        }
+        return answer;
+    }
+}
+*/
+
+class Solution {
+    public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        answer[0] = x;
+        for (int i = 1; i < n; i++) {
+            answer[i] = answer[i-1] + x; 
         }
         return answer;
     }
