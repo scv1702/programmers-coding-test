6501f865ad8862cecf18eb1043f19822616b2619
SHIN CHANGYU <63400830+scv1702@users.noreply.github.com>
2023-07-13 16:52:10 +0900

    Solve 시저 암호

---
+++ level1/시저 암호.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String s, int n) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c != ' ') {
+                if (Character.isLowerCase(c))
+                    c = (char) ('a' + (c - 'a' + n) % 26);
+                else
+                    c = (char) ('A' + (c - 'A' + n) % 26);
+            }
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}
