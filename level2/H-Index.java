27fa923cf655f7e3cc583b3523f685bd9153bf14
신찬규(Shin Changyu) <63400830+scv1702@users.noreply.github.com>
2023-12-20 20:45:14 +0900

    Resolve H-Index

--- level2/H-Index.java
+++ level2/H-Index.java
@@ -6,7 +6,7 @@ class Solution {
         int n = citations.length;
         // h번 이상 인용된 논문이 h편 이상이고, 나머지 논문이 h편 이하 인용됐다면 max(h)가 h-index
         Arrays.sort(citations);
-        int maxH = citations[n-1];
+        int maxH = citations[n - 1];
         for (int h = maxH; h >= 0; h--) {
             int graterThanOrEqaulToH = 0;
             for (int j = 0; j < n; j++) {
@@ -21,3 +21,22 @@ class Solution {
         return -1;
     }
 }
+
+/* 
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        Arrays.sort(citations);
+        for (int i = 0; i < citations.length; i++) {
+            int h = citations.length - i;
+            if (citations[i] >= h) { // h번 이상 인용된 논문이 h편 이상인 경우
+                answer = h;
+                break;
+            }
+        }
+        return answer;
+    }
+}
+*/
